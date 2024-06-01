a6b3dc028792e609c979bc8a9bd76260b8170c63
Valentina Vera Paz <valenverpaz@gmail.com>
2024-06-01 15:42:00 -0500

    Initial Commit

---
+++ SwiftUIBeta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIBeta
+//
+//  Created by Valentina Vera Paz on 1/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIBeta/SwiftUIBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBetaApp.swift
+//  SwiftUIBeta
+//
+//  Created by Valentina Vera Paz on 1/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
